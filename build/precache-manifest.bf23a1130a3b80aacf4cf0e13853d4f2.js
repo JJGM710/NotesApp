a0ffabc09c337bf2f3eae6a9263cc358923dd4e2
Juan Giler <juangiler7@outlook.com>
2020-09-07 11:49:29 -0500

    build commit

---
+++ build/precache-manifest.bf23a1130a3b80aacf4cf0e13853d4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1983747ca5c16dcd9e4f36d153bdb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e7c794bc31e990c5a2",
+    "url": "/static/css/main.61577f37.chunk.css"
+  },
+  {
+    "revision": "b5790a2045339bb196c9",
+    "url": "/static/js/2.4824bf09.chunk.js"
+  },
+  {
+    "revision": "3f6acc3de81d98392acef70f68db9b5f",
+    "url": "/static/js/2.4824bf09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e7c794bc31e990c5a2",
+    "url": "/static/js/main.36159354.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
